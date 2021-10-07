d8208e42b43b77b5983c35dfaca6a7e8d94bac01
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-10-07 15:54:57 -0700

    passing POST test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool.js');
 const setup = require('../data/setup.js');
 const request = require('supertest');
 const app = require('../lib/app.js');
+// const UserService = require('../lib/services/UserService.js');
 
 describe('complex-apis routes', () => {
   beforeEach(() => {
@@ -15,10 +16,11 @@ describe('complex-apis routes', () => {
   it('signs up a user with POST', async () => {
     const res = await request(app)
       .post('/api/auth/signup')
-      .send({ email: 'a@aaa.com', password: '1234' });
+      .send({ email: 'a@aaa.com', pwd: '1234' });
 
     expect(res.body).toEqual({
-      id: expect.any(String),
+      id: '1',
+      email: 'a@aaa.com',
     });
   });
 });
